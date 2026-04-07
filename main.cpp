f75b0fee50a4365379a7ef16fad42bf8667bbf43
Yhr <23301142@bjtu.edu.cn>
2026-04-07 15:41:10 +0800

    复合变换测试

--- main.cpp
+++ main.cpp
@@ -1,46 +1,96 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <cmath>
 #include "MyMath.h"
 
+const char* vertexShaderSource = "#version 330 core\n"
+                                 "layout (location = 0) in vec3 aPos;\n"
+                                 "uniform mat4 transform;\n"
+                                 "void main()\n"
+                                 "{gl_Position = transform * vec4(aPos.x, aPos.y, aPos.z, 1.0);}\0";
+const char* fragmentShaderSource ="#version 330 core\n"
+                                  "out vec4 FragColor;\n"
+                                  "void main(){FragColor = vec4(1.0f,0.5f,0.2f,1.0f);}\0";
+
+
 int main() {
-    if (!glfwInit()) return -1;
-    GLFWwindow* window = glfwCreateWindow(600, 600, "2D Transformation Test", NULL, NULL);
-    if (!window) {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Ex1", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
     glfwMakeContextCurrent(window);
 
-    while (!glfwWindowShouldClose(window)) {
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader,1,&vertexShaderSource,NULL);
+    glCompileShader(vertexShader);
+
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader,1,&fragmentShaderSource,NULL);
+    glCompileShader(fragmentShader);
+
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram,vertexShader);
+    glAttachShader(shaderProgram,fragmentShader);
+    glLinkProgram(shaderProgram);
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    float vertices[] = {
+            -0.5f, -0.5f, 0.0f,
+            0.5f, -0.5f, 0.0f,
+            0.0f,  0.5f, 0.0f
+    };
+
+    unsigned int VBO, VAO;
+    glGenVertexArrays(1,&VAO);
+    glGenBuffers(1,&VBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER,VBO);
+    glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,3*sizeof (float ),(void *)0);
+    glEnableVertexAttribArray(0);
+
+    float timeValue = 0.0f;
+    while(!glfwWindowShouldClose(window))
+    {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // 获取时间来做动态旋转的变量
-        float timeValue = glfwGetTime();
-
-        // 复合变换测试 M = T * R * S
+        glUseProgram(shaderProgram);
+        timeValue = glfwGetTime();
         Mat4 S = Mat4::scale(0.5f, 0.5f, 1.0f);
         Mat4 R = Mat4::rotateZ(timeValue);
-        Mat4 T = Mat4::translate(1.0f, 1.0f, 0.0f);
+        Mat4 T = Mat4::translate(0.5f, 0.5f, 0.0f);
 
-        Mat4 transform = T * R * S;
+        Mat4 myTransform = T * R * S;
+        int transformLoc = glGetUniformLocation(shaderProgram,"transform");
+        glUniformMatrix4fv(transformLoc,1,GL_FALSE,myTransform.m);
 
-        glMatrixMode(GL_MODELVIEW);
-        glLoadMatrixf(transform.m);
-
-        glBegin(GL_TRIANGLES);
-        glColor3f(1.0f, 0.0f, 0.0f);
-        glVertex2f(0.0f, 0.5f);
-        glColor3f(0.0f, 1.0f, 0.0f);
-        glVertex2f(-0.5f, -0.5f);
-        glColor3f(0.0f, 0.0f, 1.0f);
-        glVertex2f(0.5f, -0.5f);
-        glEnd();
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES,0,3);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
     glfwTerminate();
     return 0;
 }
